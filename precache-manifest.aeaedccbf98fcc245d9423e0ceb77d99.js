5b1a595f1cd24bc4bb7b31b35e985b88c050e9c3
Patrick Yu <fyyu69@outlook.com>
2020-09-19 17:35:26 -0700

    add react build files

---
+++ precache-manifest.aeaedccbf98fcc245d9423e0ceb77d99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98bc88f61892659aec49d602c0d053d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a6de33077a3b4750263",
+    "url": "/static/css/main.ca54acd0.chunk.css"
+  },
+  {
+    "revision": "1ed5f05ea273c29d7635",
+    "url": "/static/js/2.8a22ffe8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8a22ffe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a6de33077a3b4750263",
+    "url": "/static/js/main.8bf87a2c.chunk.js"
+  },
+  {
+    "revision": "31de383a84f0b531942d",
+    "url": "/static/js/runtime-main.408e7f6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
